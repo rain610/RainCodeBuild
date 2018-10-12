66ff487ff8b94b370b5d99055e8bf32f2d934a5c
魏中雨 <wzy08277@ly.com>
2018-10-12 17:10:54 +0800

    add demo

--- MyFirstDemo/MyFirstDemo/Program.cs
+++ MyFirstDemo/MyFirstDemo/Program.cs
@@ -7,10 +7,25 @@ namespace MyFirstDemo
     {
         static void Main(string[] args)
         {
-            var dalRepository = new CustomerRepository();
-            var list = dalRepository.GetCustomers();
-            Console.WriteLine("Hello World!");
+            //构造函数继承
+            var test = new Test("lalal");
+            //var dalRepository = new CustomerRepository();
+            //var list = dalRepository.GetCustomers();
 
+            //索引器
+            var indexer = new IndexerExample();
+            var aa = indexer[0];
+            var indexer6 = new IndexerExampleFor6<string>();
+            indexer6.Add("666");
+            var bb = indexer6[0];
+            var indexer7 = new IndexerExample7<string>();
+            indexer7[0] = "Hello world";
+            var cc = indexer7[0];
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
